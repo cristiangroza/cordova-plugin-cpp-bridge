acefd47f9f7cf46fac567cf84744133d1e8d9529
Cristian Groza <cristiangroza9@gmail.com>
2019-07-14 13:42:22 +0300

    Remove params

--- hooks/after_prepare.js
+++ hooks/after_prepare.js
@@ -454,8 +454,6 @@ var AndroidManager = function(context, platformInfo, cppDir, classDefine, header
                 releaseStringList.push(createContentWithTemplate(cpp_releaseStringTemplate, {'INDEX': i}));
             }
         }
-        toCppParamList.push("env");
-        toCppParamList.push("instance");
 
         var cppMethod = ['Java'].concat(javaPackage).concat([bridgeFileName, className + '0new']).join('_');
 
